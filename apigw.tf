757f13ed3d044daa89af42ce1d09a8e51f8ea714
Nikita Wootten <nikita.wootten@nist.gov>
2022-09-09 15:34:58 -0400

    Redefine terraform APIGW spec using resources

--- apigw.tf
+++ apigw.tf
@@ -1,70 +1,6 @@
 resource "aws_api_gateway_rest_api" "gw" {
   name        = "${local.prefix}-gw"
   description = "The API Gateway for the ${module.vars.env.member_name} member"
-  body        = <<EOT
-    {
-      "swagger" : "2.0",
-      "info" : {
-        "version" : "2022-08-16T21:58:05Z",
-        "title" : "apigw-proxy-test"
-      },
-      "host" : "ayk7nyknel.execute-api.us-east-1.amazonaws.com",
-      "basePath" : "/prod",
-      "schemes" : [ "https" ],
-      "paths" : {
-        "/{proxy+}" : {
-          "x-amazon-apigateway-any-method" : {
-            "produces" : [ "application/json" ],
-            "parameters" : [ {
-              "name" : "proxy",
-              "in" : "path",
-              "required" : true,
-              "type" : "string"
-            } ],
-            "responses" : {
-              "200" : {
-                "description" : "200 response",
-                "schema" : {
-                  "$ref" : "#/definitions/Empty"
-                },
-                "headers" : {
-                  "Content-Type" : {
-                    "type" : "string"
-                  }
-                }
-              }
-            },
-            "x-amazon-apigateway-integration" : {
-              "credentials" : "${aws_iam_role.apigw-proxy.arn}",
-              "httpMethod" : "GET",
-              "uri" : "arn:aws:apigateway:us-east-1:s3:path/${module.s3_content_bucket.s3_bucket_id}/{proxy}",
-              "responses" : {
-                "default" : {
-                  "statusCode" : "200",
-                  "responseParameters" : {
-                    "method.response.header.Content-Type" : "integration.response.header.Content-Type"
-                  }
-                }
-              },
-              "requestParameters" : {
-                "integration.request.path.proxy" : "method.request.path.proxy"
-              },
-              "passthroughBehavior" : "when_no_match",
-              "cacheNamespace" : "878fmp",
-              "cacheKeyParameters" : [ "method.request.path.proxy", "integration.request.path.proxy" ],
-              "type" : "aws"
-            }
-          }
-        }
-      },
-      "definitions" : {
-        "Empty" : {
-          "type" : "object",
-          "title" : "Empty Schema"
-        }
-      }
-    }
-  EOT
   tags        = local.tags
 }
 
@@ -72,7 +8,17 @@ resource "aws_api_gateway_deployment" "gw-deployment" {
   rest_api_id = aws_api_gateway_rest_api.gw.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.gw.body))
+    redeployment = sha1(jsonencode([
+      # s3 integration
+      aws_api_gateway_resource.s3.id,
+      aws_api_gateway_method.s3.id,
+      aws_api_gateway_integration.s3.id,
+      aws_api_gateway_integration_response.s3.id,
+      # lambda integration
+      aws_api_gateway_resource.lambda.id,
+      aws_api_gateway_method.lambda.id,
+      aws_api_gateway_integration.lambda.id
+    ]))
   }
 
   lifecycle {
@@ -87,56 +33,107 @@ resource "aws_api_gateway_stage" "gw-stage" {
   tags          = local.tags
 }
 
-# resource "aws_apigatewayv2_api" "gw" {
-#   name          = "${local.prefix}-gw"
-#   protocol_type = "HTTP"
+#############################
+# S3 Integration Definition #
+#############################
 
-#   # cors_configuration = {
-#   #   allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
-#   #   allow_methods = ["*"]
-#   #   allow_origins = ["*"]
-#   # }
 
-#   tags = local.tags
-# }
+resource "aws_api_gateway_resource" "s3" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  parent_id   = aws_api_gateway_rest_api.gw.root_resource_id
+  path_part   = "{proxy+}"
+}
 
-# resource "aws_apigatewayv2_authorizer" "cognito" {
-#   api_id           = aws_apigatewayv2_api.gw.id
-#   name             = "${local.prefix}-identity-authorizer"
-#   authorizer_type  = "JWT"
-#   identity_sources = ["$request.header.Authorization"]
+resource "aws_api_gateway_method" "s3" {
+  rest_api_id   = aws_api_gateway_rest_api.gw.id
+  resource_id   = aws_api_gateway_resource.s3.id
+  http_method   = "ANY"
+  authorization = "NONE"
 
-#   jwt_configuration {
-#     # annoyingly at least one audience is required
-#     audience = ["transact"]
-#     issuer   = "https://${aws_cognito_user_pool.identity.endpoint}"
-#   }
-# }
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
 
-# resource "aws_apigatewayv2_integration" "query" {
-#   api_id           = aws_apigatewayv2_api.gw.id
-#   integration_type = "AWS_PROXY"
-#   integration_uri  = aws_lambda_function.auth.invoke_arn
-# }
+resource "aws_api_gateway_method_response" "s3" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  resource_id = aws_api_gateway_resource.s3.id
+  http_method = aws_api_gateway_method.s3.http_method
 
-# resource "aws_apigatewayv2_route" "query" {
-#   api_id             = aws_apigatewayv2_api.gw.id
-#   route_key          = "POST /transaction/query"
-#   authorizer_id      = aws_apigatewayv2_authorizer.cognito.id
-#   authorization_type = "JWT"
-#   target             = "integrations/${aws_apigatewayv2_integration.query.id}"
-# }
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+}
 
-# resource "aws_apigatewayv2_integration" "invoke" {
-#   api_id           = aws_apigatewayv2_api.gw.id
-#   integration_type = "AWS_PROXY"
-#   integration_uri  = aws_lambda_function.auth.invoke_arn
-# }
+resource "aws_api_gateway_integration" "s3" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  resource_id = aws_api_gateway_resource.s3.id
+  http_method = aws_api_gateway_method.s3.http_method
 
-# resource "aws_apigatewayv2_route" "invoke" {
-#   api_id             = aws_apigatewayv2_api.gw.id
-#   route_key          = "POST /transaction/invoke"
-#   authorizer_id      = aws_apigatewayv2_authorizer.cognito.id
-#   authorization_type = "JWT"
-#   target             = "integrations/${aws_apigatewayv2_integration.invoke.id}"
-# }
+  integration_http_method = "GET"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:us-east-1:s3:path/${module.s3_content_bucket.s3_bucket_id}/{proxy}"
+
+  credentials          = aws_iam_role.apigw-proxy.arn
+  passthrough_behavior = "WHEN_NO_MATCH"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "s3-integration-response" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  resource_id = aws_api_gateway_resource.s3.id
+  http_method = aws_api_gateway_method.s3.http_method
+
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Content-Type" = "integration.response.header.Content-Type"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.s3
+  ]
+}
+
+#################################
+# Lambda Integration Definition #
+#################################
+
+resource "aws_api_gateway_resource" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  parent_id   = aws_api_gateway_rest_api.gw.root_resource_id
+
+  path_part = "transaction"
+}
+
+resource "aws_api_gateway_method" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  resource_id = aws_api_gateway_resource.lambda.id
+
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.gw.id
+  resource_id = aws_api_gateway_resource.lambda.id
+  http_method = aws_api_gateway_method.lambda.http_method
+
+  uri                     = aws_lambda_function.query.invoke_arn
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+}
+
+resource "aws_lambda_permission" "lambda-permission" {
+  statement_id  = "AllowMyDemoAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.query.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.gw.execution_arn}/*/*/*"
+}

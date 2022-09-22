04524c8f3bd3824ea94d9e0fbf2300552fc48089
Alexander Stein <alexander.stein@nist.gov>
2022-09-22 10:17:05 -0400

    Add data calls for Cognito user pool and client settings.

--- cognito.tf
+++ cognito.tf
@@ -1,7 +1,15 @@
-resource "aws_cognito_user_pool" "identity" {
-  name = "${local.prefix}-identity-userpool"
-  tags = local.tags
+data "aws_cognito_user_pools" "identity" {
+  name = "blossom_test"
 }
+
+data "aws_cognito_user_pool_clients" "identity" {
+  user_pool_id = aws_cognito_user_pool.identity.id
+}
+
+# resource "aws_cognito_user_pool" "identity" {
+#   name = "${local.prefix}-identity-userpool"
+#   tags = local.tags
+# }
 
 # resource "aws_cognito_user_pool_client" "identity" {
 #   name         = "${local.prefix}-identity-userpool-client"

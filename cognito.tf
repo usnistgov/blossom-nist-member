db8c4189477434bf888598117c06e82a41adcdef
Alexander Stein <alexander.stein@nist.gov>
2022-09-22 11:39:28 -0400

    Woops, pool/pools switch for data object vs resource object.

--- cognito.tf
+++ cognito.tf
@@ -3,15 +3,5 @@ data "aws_cognito_user_pools" "identity" {
 }
 
 data "aws_cognito_user_pool_clients" "identity" {
-  user_pool_id = aws_cognito_user_pool.identity.id
+  user_pool_id = data.aws_cognito_user_pools.identity.id
 }
-
-# resource "aws_cognito_user_pool" "identity" {
-#   name = "${local.prefix}-identity-userpool"
-#   tags = local.tags
-# }
-
-# resource "aws_cognito_user_pool_client" "identity" {
-#   name         = "${local.prefix}-identity-userpool-client"
-#   user_pool_id = aws_cognito_user_pool.identity.id
-# }

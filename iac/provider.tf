638ac3d38b51b83f0384b49a51a67566024b1e44
Nikita Wootten <nikita.wootten@nist.gov>
2023-03-10 16:12:07 -0500

    IaC improvements
    * `ter.sh` wrapper script checks creds before running terraform
    * Makefiles wrap all builds & terraform operations
    * Terraform no longer local-execs builds

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,5 +1,10 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    # variables cannot be used here
+    bucket = "nist-blossom-iac"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,4 +13,10 @@ terraform {
   }
 }
 
-provider "aws" {}
+provider "aws" {
+  region = var.aws_region
+  # cannot be specified on our config due to permission issues :/
+  # default_tags {
+  #   tags = local.tags
+  # }
+}
